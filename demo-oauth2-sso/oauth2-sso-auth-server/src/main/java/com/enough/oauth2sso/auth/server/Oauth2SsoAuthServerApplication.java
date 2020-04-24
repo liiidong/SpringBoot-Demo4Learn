8a9dd0dce3029c45cf615d1b8da8ad95114f9fb6
lidong290 <lidong290@crc.com.hk>
2020-04-24 18:05:34 +0800

    添加auto2案例工程

---
+++ demo-oauth2-sso/oauth2-sso-auth-server/src/main/java/com/enough/oauth2sso/auth/server/Oauth2SsoAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.enough.oauth2sso.auth.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2SsoAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2SsoAuthServerApplication.class, args);
+    }
+
+}
