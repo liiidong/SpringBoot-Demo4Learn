1acacddc2e091364f21f9cbbac232d98ae5a96ac
lidong290 <lidong290@crc.com.hk>
2020-04-27 17:50:52 +0800

    添加springcloud-config-demo

---
+++ demo-springcloud-config/config-server/src/main/java/com/enough/configserver/ConfigServerApplication.java
@@ -0,0 +1,14 @@
+package com.enough.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
