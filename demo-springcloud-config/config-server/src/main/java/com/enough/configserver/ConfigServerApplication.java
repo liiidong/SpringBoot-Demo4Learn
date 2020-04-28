797e3415a7f3e093765db219bf5393fbec157836
lidong290 <lidong290@crc.com.hk>
2020-04-28 18:13:10 +0800

    添加配置client读取配置中心配置

--- demo-springcloud-config/config-server/src/main/java/com/enough/configserver/ConfigServerApplication.java
+++ demo-springcloud-config/config-server/src/main/java/com/enough/configserver/ConfigServerApplication.java
@@ -2,6 +2,7 @@ package com.enough.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
