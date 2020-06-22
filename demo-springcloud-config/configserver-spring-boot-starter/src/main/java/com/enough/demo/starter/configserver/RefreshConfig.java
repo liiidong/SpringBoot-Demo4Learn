a7389387a73f6ddb38e4bd751a38a61658130991
liiidong <liiidong@outlook.com>
2020-06-22 18:04:42 +0800

    删除启动类EasyPoiAutoConfiguration.class排除项（common-utils替换了最新的easypoi，放弃使用springboot的easypoi的starter）

--- demo-springcloud-config/configserver-spring-boot-starter/src/main/java/com/enough/demo/starter/configserver/RefreshConfig.java
+++ demo-springcloud-config/configserver-spring-boot-starter/src/main/java/com/enough/demo/starter/configserver/RefreshConfig.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.refresh.ContextRefresher;
 import org.springframework.context.ApplicationContext;
-import org.springframework.stereotype.Component;
 
 import java.util.Collection;
 import java.util.HashSet;
